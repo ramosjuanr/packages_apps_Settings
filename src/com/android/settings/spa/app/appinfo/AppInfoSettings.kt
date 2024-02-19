1e0508b3a2e63bad233e84e020a0826ee1086581
Juan <jramos69@gmail.com>
2024-02-19 00:25:31 -0500

    Settings: Add package name to installed app details [1/2]
    Change-Id: I9656b4b52e841b767e478a32c56e11cbf3d0d6c0

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -179,6 +179,7 @@ private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
             AppInstallerInfoPreference(app)
         }
         appInfoProvider.FooterAppVersion()
+        appInfoProvider.FooterAppPackageName()
     }
 }
 
